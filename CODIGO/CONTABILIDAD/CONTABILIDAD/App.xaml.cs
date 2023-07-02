a064f762ea2183ee92e5cd13145f6c7da6c9f8cb
INAPOLI17 <apolinarar72@gmail.com>
2023-07-01 23:58:24 -0400

    INICIO
    PLAN

---
+++ CODIGO/CONTABILIDAD/CONTABILIDAD/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CONTABILIDAD;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CODIGO/CONTABILIDAD/CONTABILIDAD/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CONTABILIDAD;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
